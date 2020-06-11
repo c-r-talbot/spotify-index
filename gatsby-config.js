4f0fbe8adce1cb0f8db5fa37a6880efc98cafe20
ct <ct@gerudo.local>
2020-06-11 12:06:01 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-spotify`,
+      options: {
+        clientId: `f2c780eb1c7b4ba0b1c470370f9865b3`,
+        clientSecret: `9091acf29d604a648dee7b437e25dfe5`,
+        refreshToken: `AQAPc9beZe70R9nT9KDKG7XRqP3JcsCxNNaEkHuOLGRblUIlAyVaDoxJWyqMAI-yY9oLx1uFN9Xmzz6oB5M72SZT6_3gKXflEbwWKEQiSI4X9I53hWyZp7lPVNyWdm2cX0Q`,
+    
+        fetchPlaylists: true, // optional. Set to false to disable fetching of your playlists
+        fetchRecent: true, // optional. Set to false to disable fetching of your recently played tracks
+        timeRanges: ['short_term', 'medium_term', 'long_term'], // optional. Set time ranges to be fetched
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,47 @@
 import React from "react"
 
+// async function postData(url = '', data ={}){
+// const response = await fetch(url, {
+//   method: 'GET',
+//   headers: new Headers({
+//     'Content-Type: application/json',
+//     
+//   }),
+// })
+// }
+
+// async function postData(url = '', data = {}, offset= 0) {
+//   // Default options are marked with *
+//   const response = await fetch(url, {
+//     method: 'GET', // *GET, POST, PUT, DELETE, etc.
+//     // mode: 'cors', // no-cors, *cors, same-origin
+//     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     // credentials: 'same-origin', // include, *same-origin, omit
+//     headers: new Headers({
+//       'Content-Type': 'application/json',
+//       'Authorization':'Bearer BQCt9U0VlcdmTslUfH9NPob5N1S2DzSajHfypImEioD6OHkou_b_OfmiGFp4bJhrVjVO5jCJsIubsRGe8fANrqdsz6f1PraxSIBKVXUhko_ghaSltXLiqKuxKiHYuZuEiSho27XYVHmEKHTHBraQBPc'
+//     }),
+//     redirect: 'follow', // manual, *follow, error
+//     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//   });
+//   return response.json(); // parses JSON response into native JavaScript objects
+// }
+
+// postData('https://api.spotify.com/v1/me/albums?limit=50')
+//   .then(data => {
+//     console.log(data); // JSON data parsed by `response.json()` call
+//     let fetched=0
+//     while(fetched < data.total){
+//       postData('https://api.spotify.com/v1/me/albums?offset=50limit=50')
+//       .then(data =>{
+//         console.log(data);
+//       })
+//       fetched+=50;
+//     }
+//   });
+
 export default function Home() {
-  return <div>Hello world!</div>
+  console.log("hey!!!!!")
+
+  return <div>Hello world!</div>  
 }
